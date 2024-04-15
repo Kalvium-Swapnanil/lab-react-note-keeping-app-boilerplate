a084d11902b2d477eac7ed8c8067cd535a0cc7f7
Swapnanil Sarkar <you@example.com>
2024-04-15 14:42:49 +0530

    Changes

--- pronote/src/components/Editor.jsx
+++ pronote/src/components/Editor.jsx
@@ -7,29 +7,32 @@ class Notepad extends Component {
         super(props);
         this.handleChange = this.handleChange.bind(this);
         
-        this.state =
-        {
+        this.state = {
             value: '',
         };
     }
 
-    handleChange(e){
-        this.setState({value: e.target.value});
+    handleChange(e) {
+        this.setState({ value: e.target.value });
     }
 
-    render(){
-        return <>
-                <div className="container">
-                    <div className="input">
-                        <h3>Input</h3>
-                        <textarea className="input-text" onChange={this.handleChange} defaultValue={this.state.value}/>
-                    </div>
-                    <div className="output">
-                        <h3>Pro Note</h3>
-                        <div className="output-text">{this.state.value}</div>
-                    </div>
+    render() {
+        return (
+            <div className="container">
+                <div className="input">
+                    <h3>Input</h3>
+                    <textarea
+                        className="input-text"
+                        onChange={this.handleChange}
+                        value={this.state.value}
+                    />
                 </div>
-            </>
+                <div className="output">
+                    <h3>Pro Note</h3>
+                    <div className="output-text">{this.state.value}</div>
+                </div>
+            </div>
+        );
     }
 }
 
